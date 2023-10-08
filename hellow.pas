02589749cdda3de1d9c88ba3f6ea608a5092b467
Некрасов Алексей <nekrasovad2005@gmail.com>
2023-10-08 18:18:27 +0300

    дз

---
+++ hellow.pas
@@ -0,0 +1,7 @@
+﻿##
+println('choose your language: Русский(1) English(2) franch(3)');
+var a := readinteger;
+assert(a in 1..3);
+if a = 1 then print('Здравствуйте!');
+if a = 2 then print('Hellow!');
+if a = 3 then print('salut!');
